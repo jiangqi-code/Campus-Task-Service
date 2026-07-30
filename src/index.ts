0842ec71adb2f095cd745319e422081d0a0d07a4
userName <userEmail>
2026-07-30 09:09:19 +0800

    comit2

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3003'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
